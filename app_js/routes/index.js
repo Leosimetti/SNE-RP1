42a52790ed51fb416b4bcea9c506f79e154d59a3
supersloy <supersloy@yandex.ru>
2023-01-11 18:57:53 +0300

    refactor(js): code style and page style

--- app_js/routes/index.js
+++ app_js/routes/index.js
@@ -1,31 +1,23 @@
-var express = require('express');
-const fetch = require("node-fetch");
-var router = express.Router();
+const express = require("express");
+const api = require("../api");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-    const amount = 4
-    const randomBackground = true
-    const radio_url = "http://91.219.74.220:8000/Vanya.ogg"
-    
-    const apiKey =  process.env.apiKey
+router.get("/", async function (req, res, next) {
+  const AMOUNT = 4;
+  const randomBackground = true;
 
-    const back_pic_url = randomBackground ? (await (await fetch("https://api.waifu.pics/sfw/bully")).json()).url : 'https://i.waifu.pics/PGj1tg7.gif'
-    const cats = (await (await fetch(`https://api.thecatapi.com/v1/images/search?limit=${amount}&api_key=${apiKey}`)).json()).map((cat) => cat.url)
-    const descriptions = await (await fetch('https://binaryjazz.us/wp-json/genrenator/v1/story/' + amount)).json()
-    const posts = cats.map((catPic, index) => (
-            {
-                pic: catPic,
-                content: descriptions[index]
-            }
-        )
-    )
+  console.log({
+    posts: api.getPosts(AMOUNT),
+    img_url: api.getBackImage(randomBackground),
+    audio_src: api.RADIO_URL,
+  });
 
-    res.render('index', {
-        posts: posts,
-        img_url: back_pic_url,
-        audio_src: radio_url
-    })
+  res.render("index", {
+    posts: await api.getPosts(AMOUNT),
+    img_url: await api.getBackImage(randomBackground),
+    audio_src: api.RADIO_URL,
+  });
 });
 
 module.exports = router;
