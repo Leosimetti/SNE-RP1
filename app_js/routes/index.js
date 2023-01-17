33ea116acfee083b5e3adbda9e8cda95e21140d7
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2023-01-17 23:54:44 +0300

    feat(js): added comment section via mongo

--- app_js/routes/index.js
+++ app_js/routes/index.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const api = require("../api");
 const {URLS} = require("../api");
+const {response} = require("express");
+const mongoose = require('mongoose');
 const router = express.Router();
+mongoose.connect('mongodb://admin:admin@localhost:27017', {useNewUrlParser: true})
+const Comment = mongoose.model('Comment', new mongoose.Schema({ text: String }));
+
+
+const AMOUNT = 4;
+const randomBackground = true;
+async function load_page(res){
+    res.render("index", {
+        posts: await api.getPosts(AMOUNT),
+        comments: await Comment.find(),
+        img_url: await api.getBackImage(randomBackground),
+        audio_src: URLS.RADIO,
+    });
+}
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-    const AMOUNT = 4;
-    const randomBackground = true;
-
-    const response = {
-        posts: await api.getPosts(AMOUNT),
-        img_url: await api.getBackImage(randomBackground),
-        audio_src: URLS.RADIO,
-    }
-
-    console.log(response);
-    res.render("index", response);
+    await load_page(res)
+    console.log(res);
 });
+
+router.post("/", async function (req, res, next) {
+    const comment = req.body.comm_input
+
+    await Comment.create({ text: comment });
+    await load_page(res)
+    console.log(res);
+})
 
 module.exports = router;
