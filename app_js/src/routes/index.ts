542c8cbed21cd3909c76374b85f5f9eda2f5c78e
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2023-01-18 23:50:54 +0300

    ref(js): moved to ts

--- app_js/src/routes/index.ts
+++ app_js/src/routes/index.ts
@@ -1,22 +1,34 @@
-import express from 'express'
-import { URLS, getBackImage, getPosts } from '../api'
+import express from "express";
+import { URLS, getBackImage, getPosts } from "../api";
+import * as mongoose from "mongoose";
 
-export const router = express.Router()
+export const router = express.Router();
+mongoose.connect("mongodb://admin:admin@localhost:27017").then(r => console.log("Connected"));
+const Comment = mongoose.model("Comment", new mongoose.Schema({ text: String, pic: Number }));
+const AMOUNT = 4;
+const randomBackground = true;
+
+
+async function load_page(res) {
+  const posts = await getPosts(AMOUNT);
+  res.render("index", {
+    posts: posts,
+    comments: await Comment.find(),
+    img_url: await getBackImage(randomBackground),
+    audio_src: URLS.RADIO
+  });
+}
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  const AMOUNT = 5
-  const RANDOM_BACKGROUND = true
+router.get("/", async function(req, res, next) {
+  await load_page(res);
+  console.log(res);
+});
 
-  console.log({
-    posts: getPosts(AMOUNT),
-    img_url: getBackImage(RANDOM_BACKGROUND),
-    audio_src: URLS.RADIO,
-  })
+router.post("/", async function(req, res, next) {
+  const comment = req.body.comm_input;
 
-  res.render('index', {
-    posts: await getPosts(AMOUNT),
-    img_url: await getBackImage(RANDOM_BACKGROUND),
-    audio_src: URLS.RADIO,
-  })
-})
+  await Comment.create({ text: comment, pic: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000 });
+  await load_page(res);
+  console.log(res);
+});
