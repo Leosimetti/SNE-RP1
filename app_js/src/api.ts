56058cfb53ef2043250c64546e45ef0fad82debb
supersloy <supersloy@yandex.ru>
2023-01-20 02:53:37 +0300

    feat(app): zod validation

--- app_js/src/api.ts
+++ app_js/src/api.ts
@@ -1,16 +1,24 @@
 import fetch from 'node-fetch'
+import { z } from 'zod'
+
+const backImageZ = z.object({
+  url: z.string().url(),
+})
+const catImagesZ = z.array(
+  z.object({
+    url: z.string().url(),
+  })
+)
+const descriptionsZ = z.array(z.string().min(1).max(125))
 
 async function fetchJS(url: string, params?: string[]) {
   const finalURL = params ? `${url}?${params.join('&')}` : url
   return await (await fetch(finalURL)).json()
 }
 
-type BackImageType = { url: string }
 export async function getBackImage(isRandom: boolean) {
   if (isRandom) {
-    const dynamicImageRes = (await fetchJS(
-      URLS.DYNAMIC_IMAGE
-    )) as BackImageType
+    const dynamicImageRes = backImageZ.parse(await fetchJS(URLS.DYNAMIC_IMAGE))
     return dynamicImageRes.url
   }
   return URLS.STATIC_IMAGE
@@ -19,15 +27,14 @@ export async function getBackImage(isRandom: boolean) {
 async function getCatImages(limit: number) {
   const api_key = process.env.apiKey
   const api_key_param = api_key ? [`api_key=${api_key}`] : []
-  const catImagesRes = await fetchJS(
-    URLS.CATS_IMAGE,
-    [`limit=${limit}`].concat(api_key_param)
+  const catImagesRes = catImagesZ.parse(
+    await fetchJS(URLS.CATS_IMAGE, [`limit=${limit}`].concat(api_key_param))
   )
   return catImagesRes.map((cat) => cat.url)
 }
 
 async function getDescriptions(amount: number) {
-  return await fetchJS(`${URLS.DESCRIPTION}/${amount}`)
+  return descriptionsZ.parse(await fetchJS(`${URLS.DESCRIPTION}/${amount}`))
 }
 
 export async function getPosts(amount: number) {
@@ -45,24 +52,9 @@ export function getEnvOrDefault(envName: string, defaultVal: string) {
 }
 
 export const URLS = {
-  RADIO: getEnvOrDefault(
-    'RADIO',
-    'http://91.219.74.220:8000/Vanya.ogg'
-  ),
-  DYNAMIC_IMAGE: getEnvOrDefault(
-    'DYNAMIC_IMAGE',
-    'https://api.waifu.pics/sfw/bully'
-  ),
-  STATIC_IMAGE: getEnvOrDefault(
-    'STATIC_IMAGE',
-    'https://i.waifu.pics/PGj1tg7.gif'
-  ),
-  CATS_IMAGE: getEnvOrDefault(
-    'CATS_IMAGE',
-    'https://api.thecatapi.com/v1/images/search'
-  ),
-  DESCRIPTION: getEnvOrDefault(
-    'DESCRIPTION',
-    'https://binaryjazz.us/wp-json/genrenator/v1/story'
-  ),
+  RADIO: getEnvOrDefault('RADIO', 'http://91.219.74.220:8000/Vanya.ogg'),
+  DYNAMIC_IMAGE: getEnvOrDefault('DYNAMIC_IMAGE', 'https://api.waifu.pics/sfw/bully'),
+  STATIC_IMAGE: getEnvOrDefault('STATIC_IMAGE', 'https://i.waifu.pics/PGj1tg7.gif'),
+  CATS_IMAGE: getEnvOrDefault('CATS_IMAGE', 'https://api.thecatapi.com/v1/images/search'),
+  DESCRIPTION: getEnvOrDefault('DESCRIPTION', 'https://binaryjazz.us/wp-json/genrenator/v1/story'),
 }
