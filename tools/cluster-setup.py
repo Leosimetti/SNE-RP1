5a08ec1f97669ba85ac3dad55fd313d9ee1fcbd3
m.a.olokin <m.a.olokin@tinkoff.ru>
2023-01-21 19:37:51 +0300

    feat(tools): script fails on errors, key_shares and key_threshold may vary

--- tools/cluster-setup.py
+++ tools/cluster-setup.py
@@ -3,9 +3,12 @@ import subprocess
 import sys
 
 def run_command(cmd: str) -> str:
-    return subprocess.run(
-        cmd.split(), capture_output=True
-    ).stdout.decode("utf-8")
+    out = subprocess.run(cmd.split(), capture_output=True)
+    if out.returncode != 0:
+        print(out.stderr.decode("utf-8"))
+        exit(out.returncode)
+    else:
+        return out.stdout.decode("utf-8")
 
 if len(sys.argv[1:]) < 1: 
     print("Please specify the API key in the command line arguments")
@@ -17,19 +20,25 @@ run_command(r"helm install vault charts/rp-vault/ --values charts/values.yaml --
 print("waiting for pods")
 run_command(r"kubectl wait --namespace=vault --for=condition=Ready pod/vault-0")
 
+key_shares = 5
+key_threshold = 3
+
+assert key_shares >= key_threshold
+
 print("initializing Vault")
-init_out = run_command(r"kubectl -n vault exec -it statefulsets/vault -- vault operator init -n 1 -t 1 -format json")
+init_out = run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault operator init -key-shares {key_shares} -key-threshold {key_threshold} -format json")
 init_json = json.loads(init_out)
 
 root_token = init_json["root_token"]
-unseal_keys_b64 = init_json["unseal_keys_b64"][0]
-unseal_keys_hex = init_json["unseal_keys_hex"][0]
+unseal_keys_b64 = init_json["unseal_keys_b64"]
+unseal_keys_hex = init_json["unseal_keys_hex"]
 
 print(f"obtained root token: {root_token}")
-print(f"obtained unseal key: {unseal_keys_b64}")
+print("obtained unseal keys:\n\t{}".format("\n\t".join(unseal_keys_b64)))
 
 print("unsealing")
-run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault operator unseal {unseal_keys_b64}")
+for key in unseal_keys_b64[:key_threshold]: 
+    run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault operator unseal {key}")
 
 print("logging in")
 run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault login {root_token}")
