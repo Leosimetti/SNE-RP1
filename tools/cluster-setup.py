6fdadd8e73af270bd067c07314fe41fb07b67e4a
m.a.olokin <m.a.olokin@tinkoff.ru>
2023-03-10 22:12:26 +0300

    ref(script): logging

--- tools/cluster-setup.py
+++ tools/cluster-setup.py
@@ -2,7 +2,8 @@ import json
 import subprocess
 import sys
 
-def run_command(cmd: str) -> str:
+def run_command(description: str, cmd: str) -> str:
+    print(description)
     out = subprocess.run(cmd.split(), capture_output=True)
     stdout = out.stdout.decode("utf-8")
     if out.returncode != 0:
@@ -17,19 +18,24 @@ if len(sys.argv[1:]) < 1:
     print("Please specify the API key in the command line arguments")
     exit(1)
 
-print("Installing the Vault Helm chart")
-run_command(r"helm install vault charts/rp-vault/ --values charts/values.yaml --wait")
+run_command(
+    "Installing the Vault Helm chart",
+    r"helm install vault charts/rp-vault/ --values charts/values.yaml --wait"
+)
 
-print("waiting for pods")
-run_command(r"kubectl wait --namespace=vault --for=condition=Ready pod/vault-0")
+run_command(
+    "waiting for pods",
+    r"kubectl wait --namespace=vault --for=condition=Ready pod/vault-0"
+)
 
 key_shares = 5
 key_threshold = 3
-
 assert key_shares >= key_threshold
 
-print("initializing Vault")
-init_out = run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault operator init -key-shares {key_shares} -key-threshold {key_threshold} -format json")
+init_out = run_command(
+    "initializing Vault",
+    f"kubectl -n vault exec -it statefulsets/vault -- vault operator init -key-shares {key_shares} -key-threshold {key_threshold} -format json"
+)
 init_json = json.loads(init_out)
 
 root_token = init_json["root_token"]
@@ -39,26 +45,40 @@ unseal_keys_hex = init_json["unseal_keys_hex"]
 print(f"obtained root token: {root_token}")
 print("obtained unseal keys:\n\t{}".format("\n\t".join(unseal_keys_b64)))
 
-print("unsealing")
-for key in unseal_keys_b64[:key_threshold]: 
-    run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault operator unseal {key}")
+for i, key in enumerate(unseal_keys_b64[:key_threshold]): 
+    run_command(
+        f"unsealing #{i}",
+        f"kubectl -n vault exec -it statefulsets/vault -- vault operator unseal {key}"
+    )
 
-print("logging in")
-run_command(f"kubectl -n vault exec -it statefulsets/vault -- vault login {root_token}")
+run_command(
+    "logging in",
+    f"kubectl -n vault exec -it statefulsets/vault -- vault login {root_token}"
+)
 
-print("running policy creation script")
-run_command(r"kubectl -n vault exec -it statefulsets/vault -- sh /home/create-policies.sh")
+run_command(
+    "running policy creation script",
+    r"kubectl -n vault exec -it statefulsets/vault -- sh /home/create-policies.sh"
+)
 
-print("installing MongoDB Helm chart")
-run_command(r"helm install mongo charts/rp-mongo/ --namespace=vault --values charts/values.yaml --wait")
+run_command(
+    "installing MongoDB Helm chart",
+    r"helm install mongo charts/rp-mongo/ --namespace=vault --values charts/values.yaml --wait"
+)
 
-print("running mongo policy creation script")
-run_command(r"kubectl -n vault exec -it statefulsets/vault -- sh /home/create-mongo-policies.sh")
+run_command(
+    "running mongo policy creation script",
+    r"kubectl -n vault exec -it statefulsets/vault -- sh /home/create-mongo-policies.sh"
+)
 
-print("adding the API key to the vault")
-run_command(f"kubectl -n vault exec -it vault-0 -- vault kv put secret/basic-secret/helloworld apiKey={sys.argv[1]}")
+run_command(
+    "adding the API key to the vault",
+    f"kubectl -n vault exec -it vault-0 -- vault kv put secret/basic-secret/helloworld apiKey={sys.argv[1]}"
+)
 
-print("starting app")
-run_command(r"helm install app charts/rp-app/ --namespace=vault --values charts/values.yaml --wait")
+run_command(
+    "starting app",
+    r"helm install app charts/rp-app/ --namespace=vault --values charts/values.yaml --wait"
+)
 
 print("done!")
